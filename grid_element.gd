f18ce1d465e5ed0acacb9f4e223e2cae6deb437c
mmaracic <marijo.maracic@gmail.com>
2025-01-01 02:28:52 +0100

    Completing grid element

--- grid_element.gd
+++ grid_element.gd
@@ -1,10 +1,12 @@
 extends Node3D
 
+enum Puck {White, Black, None}
+
 var targeted: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	setPuck(Puck.White)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,6 +16,20 @@ func _process(delta: float) -> void:
 func setPosition(position: Vector3) -> void:
 	var marker = get_node("ElementMarker")
 	marker.position = position
+	
+func setPuck(type: Puck) -> void:
+	var whitePuck = get_node("ElementMarker/WhitePuck")
+	var blackPuck = get_node("ElementMarker/BlackPuck")
+	match type:
+		Puck.White:
+			whitePuck.visible = true
+			blackPuck.visible = false
+		Puck.Black:
+			whitePuck.visible = false
+			blackPuck.visible = true
+		Puck.None:
+			whitePuck.visible = false
+			blackPuck.visible = false
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed() and targeted:
